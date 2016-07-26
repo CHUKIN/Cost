7f01aab1ce8dda234596bcbcc11f319e2c9bfc99
chukin <dmitriyatamanchuk@gmail.com>
2016-07-26 13:20:35 +0300

    Добавление модели Расходы

--- CostApp/admin.py
+++ CostApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Group
+from .models import Group, Cost
 
 admin.site.register(Group)
+admin.site.register(Cost)
--- CostApp/models.py
+++ CostApp/models.py
@@ -1,7 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
 class Group(models.Model):
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Cost(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.IntegerField()
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    priority = models.IntegerField()
+    user = models.CharField(max_length=200)
+    date = models.DateTimeField('date published')
+
     def __str__(self):
         return self.name
